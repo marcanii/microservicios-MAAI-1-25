c47c768cf4b7564a447a6feefb22dcc69a28f1a3
marcanii <israelmarcani10@gmail.com>
2025-03-27 20:19:36 -0400

    laboratorio 2

--- Laboratorios/Lab02/Ejercicio3/server.js
+++ Laboratorios/Lab02/Ejercicio3/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// Conectar a MongoDB
+// Conectar a MongoDB usando la URI del .env
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB conectado'))
     .catch(error => console.log('Error en conexión:', error));
